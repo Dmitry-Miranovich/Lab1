9f5fe728c9cc3301f529a8fcc49f7977497ed571
Dmitry Miranovich <miranovich.2001@yandex.ru>
2020-09-20 14:41:19 +0300

    Add some comments

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,11 @@ import mainPackage.List;
 public class Main {
 
     public static void main(String[] args) {
+        Lab1();
+        Lab2();
+    }
+    static void Lab1(){
+        System.out.println("Задание №1.1");
         List<Polynomial> s = new List<>();
         List<Polynomial> p = new List<>();
         List<Polynomial> r = new List<>();
@@ -20,7 +25,24 @@ public class Main {
         }
         System.out.println("\nСумма всех членов многочлена s при x = 2: " + Meaning(s,2));
     }
+    static void Lab2(){
+        System.out.print("\nЗадание №1.3\n");
+        List<Person>book = new List<>();
+        book.add(new Person("Даркхолмович Иван Васильевич", 2991931));
+        book.add(new Person("Миранович Дмитрий Владимирович", 1998003));
+        book.add(new Person("Михаленко Даниил Власович", 3219982));
+        book.add(new Person("Харритонович Билли Власович", 0));
+        for(int i = 0; i<book.size(); i++){
+            book.get(i).ToString();
+        }
+        searchNumber(book, "Даркхолмович");
+        searchNumber(book, "Харритонович");
+        searchSurname(book, 2991931);
+        lexicographicSort(book);
 
+    }
+
+    //Сложение двух многочленов
     public static void Add(List<Polynomial> p, List<Polynomial> q, List<Polynomial> r) {
         for(int i = 0; i<q.size(); i++){
             p.add(q.get(i));
@@ -31,6 +53,7 @@ public class Main {
         Fix(p);
         BubbleSort(p);
     }
+
     //Сортировка списка по степени
     static void BubbleSort(List<Polynomial> list) {
         for(int i = list.size()-1; i>0; i--)
@@ -42,6 +65,7 @@ public class Main {
             }
         }
     }
+
     //Поиск всех элементов в списке с одинаковыми степенями с последующим их сложением
     static void Fix(List<Polynomial> list) {
         for (int i = 0; i < list.size(); i++)
@@ -55,6 +79,8 @@ public class Main {
                 }
             }
     }
+
+    //Проверка равенства многочленов
     public static boolean Equality(List<Polynomial> p, List<Polynomial> r) {
         if (p.size() == r.size()) {
             for (int i = 0; i < p.size(); i++) {
@@ -67,7 +93,7 @@ public class Main {
         return false;
     }
 
-
+    //Вычисление многочлена в данной координате
     public static int Meaning(List<Polynomial> p, int x) {
         int sum = 0;
         for(int i = 0; i<p.size(); i++){
@@ -76,5 +102,58 @@ public class Main {
             sum+= degree*part.getA();
         }
         return sum;
+    }
+
+    //Методы для задания №1.3
+
+    //Сортировка по алфавитному порядку
+    static void lexicographicSort(List<Person>list) {
+        for (int i = list.size()-1; i > 0; i--)
+            for (int j = 0; j < i; j++) {
+                Person temp = list.get(j);
+                char fLetter = list.get(j).getPersonInfo().charAt(0);
+                char sLetter = list.get(j + 1).getPersonInfo().charAt(0);
+                if (indexInAlphabet(fLetter) > indexInAlphabet(sLetter)) {
+                    list.set(list.get(j + 1), j);
+                    list.set(temp, j + 1);
+                }
+            }
+    }
+
+    //Поиск номера в алфавите
+    private static int indexInAlphabet(char letter){
+        char []Alphabet = "АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ".toCharArray();
+        int index = 0;
+        for(int i = 0; i<Alphabet.length; i++)
+            if(Alphabet[i] == letter){
+                index = i;
+                break;
+            }
+        return index;
+    }
+
+    //Поиска фамилии по номеру телефона
+    public static void searchSurname(List<Person> list, int number){
+        for(int i = 0; i< list.size(); i++){
+            if(list.get(i).getNumber() == number){
+                System.out.printf("Фамилия по номеру телефона(%d): %s\n",number ,findSurname(list.get(i).getPersonInfo()));
+                break;
+            }
+        }
+    }
+
+    //Поиск номера телефона по фамилии
+    public static void searchNumber(List<Person> list, String name){
+        for(int i = 0; i<list.size(); i++){
+            if(findSurname(list.get(i).getPersonInfo()).equalsIgnoreCase(name)){
+                System.out.printf("Номер телефона по фамилии(%s): %d\n",name,list.get(i).getNumber());
+                break;
+            }
+        }
+    }
+
+    public static String findSurname(String ID){
+        String []mas = ID.split("[ ]+");
+        return mas[0];
     }
 }
---
+++ src/Person.java
@@ -0,0 +1,32 @@
+import static java.lang.Math.*;
+public class Person {
+    private int number;
+    private String personInfo;
+    public Person(String personInfo, int number){
+       if(number<1000000)
+           number = (int) (random()* 10000000);
+       if(personInfo.split("[ ]+").length<3)
+           personInfo = "Иванов Иван Иванович";
+       this.number = number;
+       this.personInfo = personInfo;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    public String getPersonInfo() {
+        return personInfo;
+    }
+
+    public void setPersonInfo(String personInfo) {
+        this.personInfo = personInfo;
+    }
+    public void ToString(){
+        System.out.println("ФИО: "+ personInfo + ", Телефон: " + number);
+    }
+}
